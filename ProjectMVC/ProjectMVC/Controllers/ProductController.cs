675f8c7c6e29367ecdc867db31c61a7b20666fef
ManikantaVanga111 <manikantavanga111@gmail.com>
2023-12-18 16:37:19 +0530

    upload

---
+++ ProjectMVC/ProjectMVC/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProjectMVC.Models;
+
+namespace ProjectMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            List<Product> products = await GetProducts();
+
+
+            return View(products);
+        }
+
+        public async Task<List<Product>> GetProducts()
+        {
+            List<Product> product = new List<Product>();
+
+            using (HttpClient client = new HttpClient())
+            {
+
+                HttpResponseMessage response = await client.GetAsync("https://dummyjson.com/products");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonContent = await response.Content.ReadAsStringAsync();
+
+                    Product singleProduct = JsonConvert.DeserializeObject<Product>(jsonContent);
+                    product.Add(singleProduct);
+                }
+                return product;
+            }
+        }
+    }
+
+}
---
+++ ProjectMVC/ProjectMVC/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    //    public List<string> Images { get; set; }
+    }
+}
