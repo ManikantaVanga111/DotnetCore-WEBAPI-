bd39018027122f9aeaf7dddbda53ac56ab506cc8
ManikantaVanga111 <manikantavanga111@gmail.com>
2023-12-19 09:39:28 +0530

    consuming

--- ProjectMVC/ProjectMVC/Controllers/ProductController.cs
+++ ProjectMVC/ProjectMVC/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using NuGet.Packaging;
 using ProjectMVC.Models;
+using System.Collections.Immutable;
+using System.Net.NetworkInformation;
 
 namespace ProjectMVC.Controllers
 {
@@ -16,23 +19,69 @@ namespace ProjectMVC.Controllers
 
         public async Task<List<Product>> GetProducts()
         {
+            //List<ProductResponse> product = new List<ProductResponse>();
+            //  List<ProductList> p = new List<ProductList>();
             List<Product> product = new List<Product>();
+
+
+            //   List<Product> productList;
+            //  Products product;
 
             using (HttpClient client = new HttpClient())
             {
-
+                //   client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
                 HttpResponseMessage response = await client.GetAsync("https://dummyjson.com/products");
 
                 if (response.IsSuccessStatusCode)
                 {
-                    string jsonContent = await response.Content.ReadAsStringAsync();
+                    var jsonContent = await response.Content.ReadAsStringAsync();
+                    //  Product product = JsonConvert.DeserializeObject<Product>(jsonContent);
+                    ProductList productlist = JsonConvert.DeserializeObject<ProductList>(jsonContent);
+                    //foreach (var products in productlist.Products)
+                    //{
+                    // // p.Add(productlist.Products);
+                    //    Console.WriteLine($"Product ID: {products.id}");
 
-                    Product singleProduct = JsonConvert.DeserializeObject<Product>(jsonContent);
-                    product.Add(singleProduct);
+                    //    Console.WriteLine($"Title: {products.title}");
+
+                    //    Console.WriteLine($"Description: {products.description}");
+                    //    Console.WriteLine($"Price: ${products.price}");
+
+                    //    foreach (var products in productlist.Products)
+                    //        //{
+                    //        //    //Console.WriteLine($"Product ID: {product.id}");
+                    //        //    //Console.WriteLine($"Title: {product.title}");
+                    //        //    //Console.WriteLine($"Description: {product.description}");
+                    //        //    Console.WriteLine($"Price: ${products.price}");
+
+                    //        product.Add(products);
+                    //    //}
+
+
+
+
+                    //    //}
+
+                    //    //  var p=productlist.Products.ToList();
+                    //    // product = productlist.Products;
+                    //    //  product.Add(p);
+                    //}
+                    productlist.Products.ForEach(products => product.Add(products));
+
+
+                 
                 }
                 return product;
-            }
-        }
-    }
 
+            }
+                }
+        [HttpPost]
+        public IActionResult AddToFavourites(Product product)
+        {
+          //  productlist.Add(product);
+            return RedirectToAction("Index");
+        }
+
+
+    }
 }
--- ProjectMVC/ProjectMVC/Models/Product.cs
+++ ProjectMVC/ProjectMVC/Models/Product.cs
@@ -1,11 +1,23 @@
 ﻿namespace ProjectMVC.Models
 {
+
     public class Product
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public int Price { get; set; }
-    //    public List<string> Images { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public double discountPercentage { get; set; }
+        public double rating { get; set; }
+        public int stock { get; set; }
+        public string brand { get; set; }
+        public string category { get; set; }
+        public string thumbnail { get; set; }
+   
+
+    }
+    public class ProductList
+    {
+        public List<Product> Products { get; set; }
     }
 }
